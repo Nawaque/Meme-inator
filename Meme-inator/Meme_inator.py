f93ab14756f27eec0bfb6b80f80f1e6faeb0aa97
Ewan Féminis <f-ewan@outlook.com>
2022-02-12 02:21:04 -0500

    Masques

--- Meme-inator/Meme_inator.py
+++ Meme-inator/Meme_inator.py
@@ -1,28 +1,29 @@
 from PIL import Image, ImageDraw, ImageFont
-import random, os, glob
+import random, os
  
 
 
 for i in range(10):
 
-    # gfg_logo.jpeg image opened using
-    # open function and assigned to variable named img
-    img = Image.open('./TEMPLATE/bob.png')
-    width, height = img.size
-    #height = int(height*100/width)
-    #width = 100
+    #Ouvre le template à utiliser et récupère sa taille
+    img1 = Image.open('./TEMPLATE/Grus-Plan.jpg')
+    width, height = img1.size
+
+    #Si voulu, le programme peut redimensioner l'image avec une largeur définie
+    #larg = 100
+    #height = int(height*larg/width)
+    #width = larg
     #img = img.resize((width, height))
 
     # Image is converted into editable form using
     # Draw function and assigned to d1
-    d1 = ImageDraw.Draw(img)
+    d1 = ImageDraw.Draw(img1)
+
+    img2 = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+    d2 = ImageDraw.Draw(img2)
  
     # Font selection from the downloaded file
     #myFont = ImageFont.truetype('./FONTS/TEST/Kingthings_Trypewriter_2.ttf', 40)
-
-    #files = glob.glob("*.ttf")
-    #print(random.choice(files))
-    #myFont = ImageFont.truetype("./FONTS/IMPACT/"+random.choice(files), 40)
 
     #print(".\FONTS\IMPACT"+chr(92)+random.choice(os.listdir(".\FONTS\IMPACT")))
     #myFont = ImageFont.truetype(".\FONTS\TEST"+chr(92)+random.choice(os.listdir(".\FONTS\TEST")), 40)
@@ -35,13 +36,20 @@ for i in range(10):
             txt = txt+chr(random.randint(32, 126))
 
         # Decide the text location, color and font
-        myFont = ImageFont.truetype(".\FONTS\TEST"+chr(92)+random.choice(os.listdir(".\FONTS\TEST")), 40)
-        d1.text((random.randint(0, width-len(txt*20)), random.randint(0, height-40)), txt, fill =(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),font=myFont)
+        font = int(width/20)
+        myFont = ImageFont.truetype(".\FONTS\TEST"+chr(92)+random.choice(os.listdir(".\FONTS\TEST")), font)
+
+        x = random.randint(0, width-int(len(txt)*font/2))
+        y = random.randint(0, height-font)
+        d1.text((x, y), txt, fill =(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),font=myFont)
+        d2.text((x, y), txt, fill =(255, 255, 255),font=myFont)
 
     # show and save the image
     # img.show()
     
-    img.save("./RESULTS/E_results{}.png".format(i))
+    img1.save("./RESULTS/E_results{}.png".format(i))
+    img2.save("./MASKS/E_results{}.png".format(i))
+
     
     
     
--- Meme-inator/Test_interface.py
+++ Meme-inator/Test_interface.py
@@ -34,17 +34,22 @@ class App(QWidget):
 
         for i in range(10):
 
-            # gfg_logo.jpeg image opened using
-            # open function and assigned to variable named img
-            img = Image.open(""+fileName)
-            width, height = img.size
-            #height = int(height*100/width)
-            #width = 100
+            #Ouvre le template à utiliser et récupère sa taille
+            img1 = Image.open(fileName)
+            width, height = img1.size
+
+            #Si voulu, le programme peut redimensioner l'image avec une largeur définie
+            #larg = 100
+            #height = int(height*larg/width)
+            #width = larg
             #img = img.resize((width, height))
 
             # Image is converted into editable form using
             # Draw function and assigned to d1
-            d1 = ImageDraw.Draw(img)
+            d1 = ImageDraw.Draw(img1)
+
+            img2 = Image.new('RGBA', (width, height), (0, 0, 0, 0))
+            d2 = ImageDraw.Draw(img2)
  
             # Font selection from the downloaded file
             #myFont = ImageFont.truetype('./FONTS/TEST/Kingthings_Trypewriter_2.ttf', 40)
@@ -62,12 +67,14 @@ class App(QWidget):
                 # Decide the text location, color and font
                 font = int(width/20)
                 myFont = ImageFont.truetype(".\FONTS\TEST"+chr(92)+random.choice(os.listdir(".\FONTS\TEST")), font)
-                d1.text((random.randint(0, width-int(len(txt)*font/2)), random.randint(0, height-font)), txt, fill =(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),font=myFont)
 
-            # show and save the image
-            # img.show()
+                x = random.randint(0, width-int(len(txt)*font/2))
+                y = random.randint(0, height-font)
+                d1.text((x, y), txt, fill =(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),font=myFont)
+                d2.text((x, y), txt, fill =(255, 255, 255),font=myFont)
     
-            img.save("./RESULTS/E_results{}.png".format(i))
+            img1.save("./RESULTS/E_results{}.png".format(i))
+            img2.save("./MASKS/E_results{}.png".format(i))
             
 
 if __name__ == '__main__':
