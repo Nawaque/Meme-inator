300755cc93be84661c8a6ec5970fee3e3067bf2f
Ewan Féminis <f-ewan@outlook.com>
2022-02-11 22:17:24 -0500

    Update meme

--- Meme-inator/Meme_inator.py
+++ Meme-inator/Meme_inator.py
@@ -1,1 +1,19 @@
-print("Hello world");
+from PIL import Image, ImageDraw, ImageFont
+ 
+# gfg_logo.jpeg image opened using
+# open function and assigned to variable named img
+img = Image.open('./TEMPLATE/bob.png')
+ 
+# Image is converted into editable form using
+# Draw function and assigned to d1
+d1 = ImageDraw.Draw(img)
+ 
+# Font selection from the downloaded file
+myFont = ImageFont.truetype('./FONTS/IMPACT/Impact.ttf', 40)
+ 
+# Decide the text location, color and font
+d1.text((65, 170), "Sample text", fill =(255, 0, 0),font=myFont)
+ 
+# show and save the image
+img.show()
+img.save("./RESULTS/results.jpeg")
